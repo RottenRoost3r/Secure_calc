e3c028ee26cbc8831b5a90445b7677dae5743829
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-22 13:30:31 -0400

    finished login page

--- app.rb
+++ app.rb
@@ -4,9 +4,42 @@ require_relative 'calc.rb'
 enable :sessions
 
 get '/' do
-  erb :login
+  erb :login, locals:{error: ""}
 end
 
 post '/log' do
-   
+  username = params[:username]
+  password = params[:password]
+  user1 = "admin"
+  user2 = "student"
+  user3 = "jordan"
+  pass1 = "admin"
+  pass2 = "minedminds"
+  pass3 = "endicott"
+
+  if username == user1 && password == pass1
+    redirect '/calc'
+  elsif username == user2 && password == pass2
+    redirect '/calc'
+  elsif username == user3 && password == pass3
+    redirect '/calc'
+  elsif username != user1 && username != user2 && username != user3 && password != pass1 && password != pass2 && password != pass3
+		
+		error = "Inavlid Username and Password"
+		erb :login, locals:{error: "Inavlid Username and Password"}
+
+  elsif username != user1 && username != user2 && username != user3
+     
+		error = "Invalid Username"
+    erb :login, locals:{error: "Inavlid Username"}
+    
+  elsif password != pass1 && password != pass2 && password != pass3
+    
+		error = "Inavlid Password"
+    erb :login, locals:{error: "Invalid Password"}
+    
+  end
+end
+
+get "/calc" do
 end
