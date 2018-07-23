102f1b808c32d80f7ddc031d3a1235b6e3753ef1
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-22 20:19:41 -0400

    finished css, fixed division variable problem

--- calc.rb
+++ calc.rb
@@ -8,7 +8,7 @@ def work(operator, n1, n2)
     when "multiply"
         result = n1.to_i * n2.to_i
     when "divide"
-        divfunk(n1, n2)
+        result = divfunk(n1, n2)
     end
     result
 end
@@ -17,7 +17,7 @@ def divfunk(num1, num2)
     if num2.to_i == 0
         false
     else
-       numresult = num1.to_i / num2.to_i
+        numresult = num1.to_i / num2.to_i 
     end
     numresult
 end
